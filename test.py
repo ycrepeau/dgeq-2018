2449cfdddcf0abfc03d3062abaa745d990887eee
Yanik Crépeau <ycrepeau@me.com>
2019-06-18 21:01:49 -0400

    Ajout noms des circonscriptons en commentaires du tableau couverture

--- test.py
+++ test.py
@@ -8,11 +8,27 @@ import pandas as pd
 import re
 import math
 
+# Identification des partis 
+re_caq = re.compile('.*C\.A\.Q\.-É\.F\.L\.$')
+re_qs = re.compile('.*Q\.S\.$')
+re_plq = re.compile('.*P\.L\.Q\./Q\.L\.P\.$')
+re_pq = re.compile('.* P\.Q\.$')
+re_pvq = re.compile('.*P\.V\.Q\./G\.P\.Q\.$')
+re_pmlq = re.compile('.* P\.M\.L\.Q\.$')
+re_pcq = re.compile('.* P.C.Q./C.P.Q.$')
+re_npdq = re.compile('.* N\.P\.D\.Q.$')
+re_bp = re.compile('.* B\.P\.$')
+re_ea = re.compile('.* É\.A\.$')
+re_pn = re.compile('.* P\.N\.$')
+
 #Regex pour vérifier si S.V =  5A, 5B, 5C...
 re_sv = re.compile('(\d+)([A-Z]*)')
 
 # Fonctions utilitaires
 def getColor(d):
+  # Examine les résultats des principaux partis et
+  # retourne la couleurs associée au parti 
+  # ayant reçu le plus de voix
   max = 0
   color = 'gray'
   try: 
@@ -34,6 +50,9 @@ def getColor(d):
   return color
 
 def getStrength(d):
+  # Examine les résultats obtenus par les principaux partis
+  # et retourne la saturation de couleur illustrant la force
+  # du parti vainqueur.
   max = 0
   strength = 0.0
   total_votes = d['B.V.']
@@ -65,28 +84,15 @@ def getStrength(d):
   return strength
   
 def groupe_consolidation(d):
+  # Examine les identifiant de section de vote
+  # et dans le cas d'une section subdivisée en
+  # raison du très grand nombre de personnes inscrites
+  # sur la liste électorale, retourne le groupe de
+  # consolidation (5A, 5B, 5C -> 5)
   reponse = ''
   m = re_sv.match(d)
-  #print("d: {} m: {}".format(d,m.group(1)))
   if len(m.group(2)) > 0: reponse = m.group(1)
   return reponse
-
-
-
-
-# Identification des partis 
-re_caq = re.compile('.*C\.A\.Q\.-É\.F\.L\.$')
-re_qs = re.compile('.*Q\.S\.$')
-re_plq = re.compile('.*P\.L\.Q\./Q\.L\.P\.$')
-re_pq = re.compile('.* P\.Q\.$')
-re_pvq = re.compile('.*P\.V\.Q\./G\.P\.Q\.$')
-re_pmlq = re.compile('.* P\.M\.L\.Q\.$')
-re_pcq = re.compile('.* P.C.Q./C.P.Q.$')
-re_npdq = re.compile('.* N\.P\.D\.Q.$')
-re_bp = re.compile('.* B\.P\.$')
-re_ea = re.compile('.* É\.A\.$')
-re_pn = re.compile('.* P\.N\.$')
-
 
 couverture = [
   # 100-199 Estrie Centre-du-Québec
@@ -137,21 +143,21 @@ couverture = [
   '326', #Saint-Henri Saint-Anne
   '330', #Sainte-Marie - Saint-Jacques
   '332', #Westmount Saint-Louis
-  '336',
-  '338',
-  '340',
-  '344',
-  '346',
-  '350',
-  '352',
-  '356',
-  '360',
-  '364',
-  '366',
-  '370',
-  '380',
-  '390', 
-  '358', 
+  '336', #Mont-Royal Outremont
+  '338', #Acadie
+  '340', #Maurice Richard
+  '344', #Laurier-Dorion
+  '346', #Gouin
+  '350', #Mercier
+  '352', #Hochelage-Maisonneuve
+  '356', #Rosemont
+  '360', #Bourassa-Sauvé
+  '364', #Jeanne-Mance - Viger
+  '366', #Anjou Louis Riel
+  '370', #Bourget
+  '380', #Pointe-aux-Tremble
+  '390', #Lafontaine 
+  '358', #Viau
   
   # 400-499 Laval
   '460', #Chomeydeu
@@ -210,26 +216,36 @@ sections = geopandas.read_file(circ_path)
 sections['NO_SV'].astype('str')
 sections['CO_CEP'].astype('str')
 sections.rename(columns={'NO_SV':'S.V.'}, inplace=True)
-#pprint(sections.shape[0])
-#pprint(sections.columns.values.tolist())
 
+# Ne conservons que les sections de vote présentes dans les
+# circonscriptons visées.
 df_internes = sections[sections['CO_CEP'].isin(couverture)]
 sections = df_internes
+
+# Assurons nous que les sections conservées possèdent un index
+# composite uniforme
 sections.set_index(['CO_CEP', 'S.V.'], inplace=True, drop=False)
 
 
+# Trouvons le centre des sections conservées ainsi que le rectangle
+# les englobant toutes.
 center_x = sections['geometry'].unary_union.centroid.x
 center_y = sections['geometry'].unary_union.centroid.y
 bounds = sections['geometry'].total_bounds
 
-#print("bounds: {} center_x: {} center_y: {}".format(bounds, center_x, center_y))
-
+# Créons une carte (Leaflet) et centrons/zoom la selon la couverture
 m = folium.Map([center_y, center_x], tiles='stamentoner', zoom_start=9)
 m.fit_bounds([[bounds[1], bounds[0]], [bounds[3], bounds[2]]])
 
+#### Traitement des résultats ####
+
+# cumul est vide au début
 cumul = pd.DataFrame()
 
+# Pour chacune des circonscriptons de la couverture
 for co_cep in couverture:
+
+  # Lecture du fichier de données et nettoyage préliminaire.
   res_path = os.path.join('data', 'resultats-section-vote', "%s.csv"%co_cep)
   data = pd.read_csv(res_path, sep=';', encoding='latin1', dtype={'S.V.':'str', 'Code':'str'})
   data.dropna(subset=['S.V.'], inplace=True)
@@ -240,19 +256,25 @@ for co_cep in couverture:
   data['consolidation'] = data['S.V.'].apply(groupe_consolidation)
   data_subset = data[data['consolidation'] != '']
   data_subset.set_index(['CO_CEP','S.V.'], inplace=True)
-  #pprint(data_subset)
   data_consolide = data_subset.groupby('consolidation').agg({'Circonscription':'first', 'B.V.':'sum', 'B.R.':'sum'})
-  #pprint(data_consolide)
   
+  # Assurons nous que les données de résultats lues possèdent un index
+  # composite uniforme identique aux sections traitées plus haut
   data.set_index(['CO_CEP','S.V.'], inplace=True, drop=False)
-  #pprint(data)
   
+  
+  # Trouvons dans le tableau les colonnes correspondant aux
+  # résultats de chaque candidature
   candidats_list = data.columns.tolist()
   ei_pos = candidats_list.index('É.I.')
   bv_pos = candidats_list.index('B.V.')
-  #print("ei: {} bv: {}".format(ei_pos, bv_pos))
   candidats = candidats_list[ei_pos+1:bv_pos]
   
+  # Pour chaque candidature, renommer chaque colonne afin
+  # de ne conserver que l'identifiant du parti. Ainsi
+  # 'Gabriel Nadeau Dubouis Q.S' -> 'qs'
+  # 'Manon Massé Q,S.' -> 'qs'
+  # 'Phlipe Couillard P.L.Q./Q.L.P' -> 'plq'
   for candidat in candidats:
     if re_caq.match(candidat): 
       data.rename(columns={candidat:'caq'}, inplace=True)
@@ -277,54 +299,77 @@ for co_cep in couverture:
     elif re_pn.match(candidat): 
       data.rename(columns={candidat:'pn'}, inplace=True)
     else:
-      print("WARNING: candidat:'{}' not matched".format(candidat))
+      # Si on n'arrive pas à associer une candidature à un parti
+      # Cette candidature est éliminée du tableay
+      #print("WARNING: candidat:'{}' not matched".format(candidat))
       data.drop(labels=candidat, axis=1, inplace=True)
 
+  # Débarasson nous des colones inutiles
   data.drop(labels=['Circonscription', 'Date scrutin', 'Étendue', 'Secteur', 'Regroupement', 'É.I.'], axis=1, inplace=True)
   
   #Consolidation 5A, 5B, 5C... ici.
   # Sélectionner les rangée correspondants aux sections subdivisées (A, B C...)
+  
+  # Créons une nouvelle colonne avec le(s) groupe(s) de consolidation
   data['consolidation'] = data['S.V.'].apply(groupe_consolidation)
   
-  
+  # Ne sélectionnons que les rangées correspondant aux sections faisant
+  # partie d'un groupe de consolidation
   data_subset = data[data['consolidation'] != '']
   data_subset.set_index(['CO_CEP','S.V.'], inplace=True, drop=False)
-  #pprint(data_subset)
+
+  # Pour toutes les colonne, nous sommes intéressés par la somme
   f = dict.fromkeys(data_subset, 'sum')
+  
+  # Sauf pour quelques colonnes qui ont un traitement spécial
   f.pop('consolidation', None)
   f.pop('S.V.', None)
   f['CO_CEP'] = 'first'
   f['NM_MUNI'] = 'first'
 
+  #Créons un tableau des éléments consolidés en utilisant groupby
   data_consolide = data_subset.groupby('consolidation', as_index=False).agg(f)
   data_consolide.rename(columns={'consolidation':'S.V.'}, inplace=True)
   data_consolide.set_index(['CO_CEP','S.V.'], inplace=True, drop=False)
 
-  #pprint(data_consolide)
   
+  # Ajoutons les données pour cette circonscripton au tableau général
   if (cumul.size <= 0):
     cumul = data
   else:
     a = cumul.append(data, sort=False)
     cumul = a
 
+  # Ajoutons éaglement les données consolidées.
   if data_consolide.size > 0:
     a = cumul.append(data_consolide, sort=False)
     cumul = a
 
 
+# Ne conservons que les données associées au 4 principaux partis
+# ainsi que le nombre total de bulletins valide (B.V.) ainsi que
+# le nom de la municipalité où se trouve la section de vote
 striped = cumul[['plq', 'pq', 'caq', 'qs', 'B.V.', 'NM_MUNI']]
+
+# Fusionnons les données géographique (sections) avec les données de
+# résultats (striped)
 sections = sections.join(striped)
+
+# Associons une couleur et une intensité à chaque section de vote
 sections['couleur']  = sections.apply(getColor, axis=1)
 sections['strength'] = sections.apply(getStrength, axis=1)
 
-
+# On prend pas de chance, on s'assure à nouveau d'avoir un index
+# normalisé.
 sections.set_index(['CO_CEP', 'S.V.'], inplace=True, drop=False)
-#pprint(sections[['plq', 'pq', 'caq', 'qs', 'couleur', 'strength' ,'NM_MUNI']])
 
 
-folium.GeoJson(sections,
+# Ajoutons les sections (après fusion avec les résultats) à lacarte
+folium.GeoJson(
+  sections,
+  # On règle le tooltip ici pour aficher diverses infos.
   tooltip = folium.features.GeoJsonTooltip(fields=['NM_CEP', 'S.V.', 'NM_MUNI', 'qs', 'caq', 'plq', 'pq'], labels=True),
+  # La magie de la colorisation et de l'intensité se fait ici
   style_function=lambda dd: {
       'fillColor': dd['properties']['couleur'],
       'color' : 'green',
